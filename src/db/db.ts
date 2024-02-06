84945c7d28f01155b3a132e652ef8fc12a08947c
harshild-hkis <122852716+harshild-hkis@users.noreply.github.com>
2024-02-06 16:29:12 +0530

    db connection changes

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connect = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/chat_app")
+    .connect("mongodb+srv://chatapp:chatapp@cluster0.ilrizhb.mongodb.net/")
     .then(() => {
       console.log("Database connected");
     })
